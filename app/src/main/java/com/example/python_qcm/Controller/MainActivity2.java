d7854095103b68b1af5bf8ce640c8cc7b5c16a87
unknown <android@MFIN15.metz.supelec.centralesupelec.local>
2020-11-23 10:40:40 +0100

    14 question de plus

--- app/src/main/java/com/example/python_qcm/Controller/MainActivity2.java
+++ app/src/main/java/com/example/python_qcm/Controller/MainActivity2.java
@@ -11,6 +11,8 @@ import com.example.python_qcm.Model.questionbank;
 import com.example.python_qcm.R;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.Arrays;
+
 public class MainActivity2 extends AppCompatActivity {
        private   TextView E21;
        private  Button B21;
@@ -37,5 +39,22 @@ public class MainActivity2 extends AppCompatActivity {
         //this.displayquestion(questionrecent);
 
 
+    }
+    private questionbank genequestion(){
+        Question q1= new Question("Laquelle des fonctions suivantes convertit un « string » en « float » en python?", Arrays.asList("str(x)","float(x)","long(x [,base] )","int(x [,base])"),1);
+        Question q2=new Question("En python 3, que fait l’opérateur //",Arrays.asList("Division entière","Retourne le reste","Division du float par zéro","idem a ** b"),0);
+        Question q3 = new Question("En Python, laquelle des fonctions suivantes vérifie dans une chaîne de caractères que tous les caractères sont des chiffres?",Arrays.asList("isdigit()","isalnum()","capitalize()","shuffle(lst)"),0);
+        Question q4=new Question("Python est un langage _______?",Arrays.asList(" interprété","machine","compilé","binaire"),0);
+        Question q5=new Question("Quel est le type de données pour un caractère en python?",Arrays.asList("chr","char","character","python ne possède aucun type de données pour les caractères, ils sont traités comme des chaînes de caractères (String)"),3);
+        Question q6=new Question("Quelle est la fonction qui compare les éléments des deux listes?",Arrays.asList("eq(list1, list2)","len(list1, list2)","cmp(list1, list2)","max(list1, list2)"),2);
+        Question q7=new Question("Laquelle des fonctions suivantes renvoie le plus petit caractère de la chaîne str?",Arrays.asList("lower()","strip()","upper(str)","min(str)"),3);
+        Question q8=new Question("Quelle fonction est utilisée pour ouvrir le fichier en lecture en Python?",Arrays.asList("fopen(file_name, mode)","open(file_name, mode)","openfile(file_name, mode)","open_file(file_name, mode)"),1);
+        Question q9=new Question("En python, quel mot clé est utilisé pour commencer une fonction?",Arrays.asList("function","fun","def","import"),2);
+        Question q10=new Question("Lequel des opérateurs suivants en python est évalué à « true » s’il ne trouve pas de variable dans la séquence spécifiée sinon « false »?",Arrays.asList("**","is","not is ","//"),2);
+        Question q11=new Question("Quelle fonction insère un objet à un index donné dans une liste?",Arrays.asList("list.index(obj)","list.index(obj)","list.pop(obj=list[-1])","list.remove(obj)"),1);
+        Question q12= new Question("En python, quelle est la bonne méthode pour charger un module?",Arrays.asList("include math","import math","#include math.h","using math"),1);
+        Question q13=new Question("Quelle est la sortie pour 'python' [-3]",Arrays.asList("h","t","o","Erreur : index négatif"),0);
+        Question q14=new Question("Quelle sera la sortie du code suivant: print type(type(int))?",Arrays.asList("type ‘int’","type ‘int’","Error","0"),1);
+        return new questionbank(Arrays.asList(q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14));
     }
 }
