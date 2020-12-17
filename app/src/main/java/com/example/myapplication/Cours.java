ea1778f66d15f09412004d9ec069d9282884d0a6
unknown <bendioussoussama@gmail.com>
2020-12-17 16:48:47 +0100

    changement du couleur est ajout de contenu dans boucle while

--- app/src/main/java/com/example/myapplication/Cours.java
+++ app/src/main/java/com/example/myapplication/Cours.java
@@ -7,10 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.myapplication.cours.BoucleWhile;
+import com.example.myapplication.cours.Bouclefor;
+import com.example.myapplication.cours.Dictionnaire;
+import com.example.myapplication.cours.Documentation;
+import com.example.myapplication.cours.FonctionLambda;
 import com.example.myapplication.cours.Liste;
+import com.example.myapplication.cours.Math;
+import com.example.myapplication.cours.Matplotlib;
+import com.example.myapplication.cours.None;
+import com.example.myapplication.cours.Numpy;
+import com.example.myapplication.cours.POO;
+import com.example.myapplication.cours.Package;
+import com.example.myapplication.cours.Recursion;
+import com.example.myapplication.cours.RegledOr;
+import com.example.myapplication.cours.String;
+import com.example.myapplication.cours.StructureConditionel;
+import com.example.myapplication.cours.Test;
+import com.example.myapplication.cours.Tuple;
 
 public class Cours extends AppCompatActivity {
-    public final static String MESSAGE_KEY = "ganeshannt.senddata.message_key";
+
     private Button L1;
     private Button L2;
     private Button L3;
@@ -62,6 +79,158 @@ public class Cours extends AppCompatActivity {
                 liste.setClass(Cours.this, Liste.class);
                 startActivity(liste);
 
+            }
+        });
+        L2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent string = new Intent();
+               string.setClass(Cours.this, String.class);
+                startActivity(string);
+            }
+        });
+        L3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dictio = new Intent();
+                dictio.setClass(Cours.this, Dictionnaire.class);
+                startActivity(dictio);
+            }
+        });
+        L4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent numpy= new Intent();
+                numpy.setClass(Cours.this, Numpy.class);
+                startActivity(numpy);
+
+            }
+        });
+        L5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent math = new Intent();
+                math.setClass(Cours.this, Math.class);
+                startActivity(math);
+            }
+        });
+        L6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent matplotlib = new Intent();
+                matplotlib.setClass(Cours.this, Matplotlib.class);
+                startActivity(matplotlib);
+
+            }
+        });
+        L7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent boucleFor = new Intent();
+                boucleFor.setClass(Cours.this, Bouclefor.class);
+                startActivity(boucleFor);
+
+            }
+        });
+        L8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bouclewhile = new Intent();
+                bouclewhile.setClass(Cours.this, BoucleWhile.class);
+                startActivity(bouclewhile);
+            }
+        });
+        L9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent stcondi= new Intent();
+               stcondi.setClass(Cours.this, StructureConditionel.class);
+               startActivity(stcondi);
+            }
+        });
+        L10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent fctlambda = new Intent();
+                fctlambda.setClass(Cours.this, FonctionLambda.class);
+                startActivity(fctlambda);
+
+            }
+        });
+        L11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent poo= new Intent();
+                poo.setClass(Cours.this, POO.class);
+                startActivity(poo);
+
+
+            }
+        });
+        L12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent documentation = new Intent();
+                documentation.setClass(Cours.this, Documentation.class);
+                startActivity(documentation);
+            }
+        });
+        L13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tuple= new Intent();
+                tuple.setClass(Cours.this, Tuple.class);
+                startActivity(tuple);
+
+            }
+        });
+        L14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rec = new Intent();
+                rec.setClass(Cours.this, Recursion.class);
+                startActivity(rec);
+
+            }
+        });
+        L15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent test = new Intent();
+                test.setClass(Cours.this, Test.class);
+                startActivity(test);
+
+
+            }
+        });
+        L16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent none = new Intent();
+                none.setClass(Cours.this, None.class);
+                startActivity(none);
+            }
+        });
+        L17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pack = new Intent();
+                pack.setClass(Cours.this, Package.class);
+                startActivity(pack);
+            }
+        });
+        L18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regle_or = new Intent();
+                regle_or.setClass(Cours.this, RegledOr.class);
+                startActivity(regle_or);
+            }
+        });
+        L19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               finish();
             }
         });
     }
--- app/src/main/java/com/example/myapplication/cours/BoucleWhile.java
+++ app/src/main/java/com/example/myapplication/cours/BoucleWhile.java
@@ -3,14 +3,28 @@ package com.example.myapplication.cours;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
+import org.w3c.dom.Text;
+
 public class BoucleWhile extends AppCompatActivity {
+     private TextView BW;
+     private TextView BWC;
+     private TextView BWT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boucle_while);
+        BW = findViewById(R.id.bw);
+        BWC=findViewById(R.id.bwc);
+        BWT=findViewById(R.id.bwt);
+
+        BW.setText("La boucle while ,(tandis que) , comme son nom l'indique permet de répéter un bloc de code plusieur fois , il se diffère de la boucle for par le fait quel s'exécute jusqu'à atteindre une condition   .Il existe deux stucture qui utilise cette boucle .");
+        BWC.setText("la première structure , est la plus simple en utilise :\n while (condition):\n\t\tbloc d'instruction à exécuter \n.Si vous utiliser un compteur comme condition n'oublier pas de le décrémenter sinon vous risquer d'avoir une boucle infini ");
+        BWT.setText("Pour cette structure elle est caractérisé par le fait que le bloc d'instruction s'exécute au moins une seule fois, on utilise :\nwhile(True):\n\t\tbloc d'instuction \nMais cette fois-ci le bloc d'instruction contient aubligatoirement un 'break' , soit par un bloc qui contient 'if' pour assurer l'existance de la condition de sortie de la boucle .\n\n\nIl remplace en générale le bloc do-while dans d'autre language ");
+
     }
 }
