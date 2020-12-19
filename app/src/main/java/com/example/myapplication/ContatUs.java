d17afee0b5c051f889e433cadb01d7492cc9a842
unknown <bendioussoussama@gmail.com>
2020-12-19 01:03:38 +0100

    ajouter lien vers gmail

--- app/src/main/java/com/example/myapplication/ContatUs.java
+++ app/src/main/java/com/example/myapplication/ContatUs.java
@@ -3,17 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ContatUs extends AppCompatActivity {
     private Button  BretourC;
+    private Button TMAIL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_contatc_us);
      BretourC= findViewById(R.id.bretourcontact);
+     TMAIL= findViewById(R.id.tmail);
      BretourC.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
@@ -23,5 +27,21 @@ public class ContatUs extends AppCompatActivity {
              finish();
          }
      });
-    }
-}
+
+     TMAIL.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             composeEmail("assadben223@gmail.com" ,"Réclamation de problème ");
+         }
+     });
+
+            }
+    public void composeEmail(String  addresses, String subject) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+
+            startActivity(intent);
+
+    }}
--- app/src/main/java/com/example/myapplication/cours/Bouclefor.java
+++ app/src/main/java/com/example/myapplication/cours/Bouclefor.java
@@ -3,14 +3,20 @@ package com.example.myapplication.cours;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
-public class Bouclefor extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class Bouclefor extends AppCompatActivity {
+   private TextView BF;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bouclefor);
+        BF = findViewById(R.id.bf);
+        BF.setText("la boucle 'for',elle aussi comme la boucle 'while' ,permet de réaliser de nombreuses  itérations d'un bloc d'instruction ,en d'autres termes plus claires ,elle permet de répéter un bloc de code , aussi elle dite borné car on sait le nombre exacte d'itération .\n\nElle peut itérer sur tout variable donner aprés son compteur (liste , chaine de caractère...).\n\n\nLa structure générale est la suivante :\n\n\nfor element in (sequence):\n\t\t\tbloc à répéter   ");
+        BF.setTextSize(20);
     }
 }
