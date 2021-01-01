2b5c6b2b07e98e7348fbcc6c0747918205cfd63f
unknown <bendioussoussama@gmail.com>
2021-01-01 17:23:55 +0100

    j'ai ajouter les  dictionnaires

--- app/src/main/java/com/example/myapplication/cours/Dictionnaire.java
+++ app/src/main/java/com/example/myapplication/cours/Dictionnaire.java
@@ -3,14 +3,40 @@ package com.example.myapplication.cours;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
 public class Dictionnaire extends AppCompatActivity {
-
+    private TextView DIC;
+    private TextView EX1;
+    private TextView DIC2;
+    private TextView DIC3;
+    private TextView DIC4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionnaire);
+        EX1 = findViewById(R.id.ex1);
+        DIC = findViewById(R.id.dictio);
+        DIC2  =findViewById(R.id.dic2);
+        DIC3 = findViewById(R.id.newdic);
+        DIC4 = findViewById(R.id.dic4);
+        DIC.setText("Avant d'oublier, le dictionnaire est exempel de type construit .\nBon,les éléments d’une liste ou d’un tuple sont ordonnés et on accéde à un élément grâce à sa position en utilisant un numéro qu’on appelle l’indice de l’élément.\n" +
+                "\n" +
+                "Un dictionnaire en Python va aussi permettre de rassembler des éléments mais ceux-ci seront identifiés par une clé. On peut faire l’analogie avec un dictionnaire de français où on accède à une définition avec un mot.\n" +
+                "\n" +
+                "Contrairement aux listes qui sont délimitées par des crochets, on utilise des accolades pour les dictionnaires.Voici un exempel : ");
+    EX1.setText("mon_dictionnaire = {\"voiture\": \"véhicule à quatre roues\", \"vélo\": \"véhicule à deux roues\"}");
+    DIC2.setText("Il est très facile d’ajouter un élément à un dictionnaire. Il suffit d’affecter une valeur pour la nouvelle clé.\n\n\tmon_dictionnaire[\"tricycle\"] = \"véhicule à trois roues\"\n\nIl est possible de créer un dictionnaire avec des accolades qui entourent les éléments. Une autre approche possible consiste à créer un dictionnaire vide et à ajouter les éléments au fur et à mesure.\n\nnombre_de_pneus = {}\n" +
+            "nombre_de_pneus[\"voiture\"] = 4\n" +
+            "nombre_de_pneus[\"vélo\"] = 2\n\n");
+    DIC3.setText("\nnombre_de_roues = {\"voiture\": 4, \"vélo\": 2, \"tricycle\": 3}\n " +
+
+            "for i in nombre_de_roues.items():\n" +
+            "    print(i)\n");
+    DIC4.setText("\n('voiture', 4)\n" +
+            "('vélo', 2)" +"\n" +
+            "('tricycle', 3) ");
     }
 }
--- app/src/main/java/com/example/myapplication/cours/Recursion.java
+++ app/src/main/java/com/example/myapplication/cours/Recursion.java
@@ -3,14 +3,32 @@ package com.example.myapplication.cours;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
 public class Recursion extends AppCompatActivity {
-
+    private TextView REC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recursion);
+        REC = findViewById(R.id.rec);
+        REC.setText("Un algorithme est dit récursif si, à un moment, il s'appelle lui-même. Pour mieux comprendre, prenons le cas de la fonction récursive car c'est l'application de la récursivité la plus courante et que c'est celle que nous utiliserons par la suite. Par conséquent, une fonction récursive est une fonction qui s'auto-appelle. Voici un petit exemple :\n" +
+                "\n" +
+                "def f(x):\n" +
+                "    x = 2 * x\n" +
+                "    f(x)\n" +
+                "    return x\n\n" +
+                "Nous voyons bien que la fonction f s'appelle elle même à la ligne 3. En réalité, voilà comment l'ordinateur agit :\n" +
+                "\n" +
+                "def f(x):\n" +
+                "    x = 2 * x\n" +
+                "        x = 2 * x\n" +
+                "            x = 2 * x\n" +
+                "                x = 2 * x\n" +
+                "                    x = 2 * x\n" +
+                "                        ...\n" +
+                "    return x"+"\n\n\n il est parfois indispensable , vous allez le rencontrer de plus dans les algorithmes de tri ");
     }
 }
